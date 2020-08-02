8ef3425701359660b46fbcb03c9932c5942e1ef6
gua-guargia <georgiazhang221@gmail.com>
2020-08-02 15:43:41 +0800

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCr0W0oRYKheYOx6mPwUfChTDXi6g5SIDY",
+    authDomain: "tpspacefinder-3afc3.firebaseapp.com",
+    databaseURL: "https://tpspacefinder-3afc3.firebaseio.com",
+    projectId: "tpspacefinder-3afc3",
+    storageBucket: "tpspacefinder-3afc3.appspot.com",
+    messagingSenderId: "900220890314"
+    //appId: "1:900220890314:web:e9cb295ab3f3f5e84d5833",
+    //measurementId: "G-C626PKP4ER"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCr0W0oRYKheYOx6mPwUfChTDXi6g5SIDY",
+    authDomain: "tpspacefinder-3afc3.firebaseapp.com",
+    databaseURL: "https://tpspacefinder-3afc3.firebaseio.com",
+    projectId: "tpspacefinder-3afc3",
+    storageBucket: "tpspacefinder-3afc3.appspot.com",
+    messagingSenderId: "900220890314"
+    //appId: "1:900220890314:web:e9cb295ab3f3f5e84d5833",
+    //measurementId: "G-C626PKP4ER"
+  }
 };
 
 /*
